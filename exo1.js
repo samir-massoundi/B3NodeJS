0fcafb8269c9ac6bfacf1d7cef29e4dfa6873779
Samir Massoundi <samir.massoundi@ynov.com>
2021-02-23 15:40:13 +0100

    Debut second Step

--- exo1.js
+++ exo1.js
@@ -1,3 +1,5 @@
+//First Step
+
 function returnStrings(s1,s2) {
     let resultat = parseInt(s1,10)+parseInt(s2,10);
     return resultat.toString();
@@ -8,3 +10,6 @@ let string2 = "5";
 
 let result = returnStrings(string1, string2);
 console.log(result);
+
+
+//Second Step
