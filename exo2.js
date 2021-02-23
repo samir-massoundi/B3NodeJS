89ccaa3c6aea1fae16d2cef1a52b2f9916b512d5
Samir Massoundi <samir.massoundi@ynov.com>
2021-02-23 16:39:35 +0100

    Exo 2 first result

--- exo2.js
+++ exo2.js
@@ -1,11 +1,23 @@
 //Second Step
 function addNumbers(strOperation)
 {
-    arrayNumbers = strOperation.split(',')
-    console.log(arrayNumbers)
-    return "0";
+    if(strOperation!=='')
+    {
+        arrayNumbers = strOperation.split(',');
+        let result=0;
+        for (let i = 0; i < arrayNumbers.length; i++) {
+            //console.log(parseFloat(arrayNumbers[i]))
+            result+=parseFloat(arrayNumbers[i])
+        }
+        //console.log("resultat in loop:", result)
+        return result.toString();
+    }
+    else
+    {
+        return "0";
+    }
 }
 
-let num1 = '1.1,2.2';
-let result = addNumbers(num1);
-console.log(result);
+let num1 = '1.1,2.2,3.3';
+let out = addNumbers(num1);
+console.log(out);
