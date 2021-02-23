bf78edbf538c15883fca571607c97cc8698ceb6c
Samir Massoundi <samir.massoundi@ynov.com>
2021-02-23 15:33:30 +0100

    Update exo1.js

--- exo1.js
+++ exo1.js
@@ -1,6 +1,6 @@
 function returnStrings(s1,s2) {
-    let resultat = parseInt(s1)+parseInt(s2);
-    return resultat.toString;
+    let resultat = parseInt(s1,10)+parseInt(s2,10);
+    return resultat.toString();
     
 }
 let string1 = "2";
