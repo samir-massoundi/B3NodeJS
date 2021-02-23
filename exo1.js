9082a394f3cb786678eaab939b00e1f5f8fd4e20
Samir Massoundi <samir.massoundi@ynov.com>
2021-02-23 15:26:54 +0100

    Create exo1.js

---
+++ exo1.js
@@ -0,0 +1,10 @@
+function returnStrings(s1,s2) {
+    let resultat = parseInt(s1)+parseInt(s2);
+    return resultat.toString;
+    
+}
+let string1 = "2";
+let string2 = "5";
+
+let result = returnStrings(string1, string2);
+console.log(result);
