4817a413c1fec64afcb53d7a1c297faeb193cc02
Samir Massoundi <samir.massoundi@ynov.com>
2021-03-08 15:31:53 +0100

    ajoute serveur TP

---
+++ serverTP/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.end('Hello World 2');
+});
+
+app.listen(3000, () => console.log('server ready'));
